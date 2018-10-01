bc79bddeca9b78d93051458ef7744bb7124b4104
Paul Campbell <nimbleltd@gmail.com>
2018-10-01 15:34:38 -0500

    Added helper(s) which formats price, date and long entries.

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,12 @@
 module EventsHelper
+
+  def format_price(event)
+    if event.free?
+    # if event == 0  
+      # content_tag(:strong,  "Free!".upcase)
+      "Free!"
+    else
+      number_to_currency(event.price)
+    end
+  end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,6 @@
 class Event < ApplicationRecord
+  def free?
+    # price.blank? || price.zero?
+    price == 0.0
+  end
 end
